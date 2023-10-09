63c74a414efe5f46ab7e2cada67276d027b78af1
Lulu Wang <lulu.wang25@gmail.com>
2023-10-08 23:55:46 -0700

    Update license and flask code

--- README.md
+++ README.md
@@ -7,5 +7,27 @@ Powered by GPT-3.5.
 Preview Demo:
 ![Creative Generator Webapp](https://j.gifs.com/BrArEJ.gif)
 
+## Setup (local)
 
+1. Navigate into the project directory.
 
+2. Create a new virtual environment:
+
+   ```bash
+   $ python -m venv venv
+   $ . venv/bin/activate
+   ```
+
+3. Install the requirements:
+
+   ```bash
+   $ pip install -r requirements.txt
+   ```
+
+4. Run the app:
+
+   ```bash
+   $ flask run
+   ```
+
+You can see the app at [http://localhost:5000](http://localhost:5000)
--- app.py
+++ app.py
@@ -1,43 +1,77 @@
 import os
-
 import openai
+import requests
+import json
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+stable_diffusion_url = "https://stablediffusionapi.com/api/v3/text2img"
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    pictureUrl = ""
     if request.method == "POST":
         userText = request.form["userText"]
-        response = openai.Completion.create(
-            model="gpt-3.5-turbo-instruct",
-            prompt=generate_chat_prompt(userText),
-            temperature=0.6,
+        # response = openai.Completion.create(
+        #     model="gpt-3.5-turbo",
+        #     prompt=generate_chat_prompt(userText),
+        #     temperature=0.6,
+        # )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            temperature=0.7,
+            messages=[
+                {"role": "system", "content": "You are generating literary sentences based on the prompt word or words."},
+                {"role": "user", "content": "England"},
+                {"role": "assistant", "content": "As the sun set, England was lit up in a glowing light."},
+                {"role": "user", "content": "Butterfly"},
+                {"role": "assistant", "content": "A butterfly landed on my finger in the garden."},
+                {"role": "user", "content": userText}
+            ],
         )
+
+        resultText = response['choices'][0]['message']['content']
+
+        payload = json.dumps({
+            "key": "A3JZV8fcdyGJe5jO2izcRIb7sCXTmA0u3XMbPefHAJLPU6g4Hn9f7elc7OQY",
+            "prompt": resultText,
+            "negative_prompt": "nudity",
+            "width": "512",
+            "height": "512",
+            "samples": "1",
+            "num_inference_steps": "20",
+            "seed": None,
+            "guidance_scale": 7.5,
+            "safety_checker": "yes",
+            "multi_lingual": "no",
+            "panorama": "no",
+            "self_attention": "no",
+            "upscale": "no",
+            "embeddings_model": stable_diffusion_url,
+            "webhook": None,
+            "track_id": None
+        })
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        responsePicture = requests.request("POST", stable_diffusion_url, headers=headers, data=payload)
+        responseJson = responsePicture.json()
+        pictureUrl = responseJson["output"]
+        print(pictureUrl)
+
         print(response)
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("index", result=resultText, pictureUrl=pictureUrl))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
-
+    pictureUrl = request.args.get("pictureUrl")
+    return render_template("index.html", result=result, pictureUrl=pictureUrl)
 
 def generate_chat_prompt(topic):
-    return """Write a sentence based on the prompt word.
+    return """Write a proper sentence based on the prompt word.
     Prompt: England
     Sentence: As the sun set, England was lit up in a glowing light.
     Prompt: Butterfly
